e684f4365737ff1bec0d989075a42720411ac81d
rethab <rethab@rethab.ch>
2013-06-09 22:25:23 +0200

    fail on usage

--- Main.hs
+++ Main.hs
@@ -13,7 +13,7 @@ import Compression.Huffman (encode, decode)
 main :: IO ()
 main = do
     args <- getArgs
-    when (length args /= 3) (usage >> exitFailure)
+    when (length args /= 3) usage
     let mode = args !! 0
         src = args !! 1
         dst = args !! 2
@@ -27,6 +27,7 @@ usage = do putStrLn "[mode] source destination"
            let mods = map fst opts
            putStrLn " where mode is one of:"
            putStrLn $ "   " ++ show mods
+           exitFailure
 
 
 opts = [("enc", encode), ("comp", compress), ("encomp", encode . compress)
