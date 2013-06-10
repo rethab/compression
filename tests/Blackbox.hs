4c9c82b06c705710fadf90416102323ab36e1336
rethab <rethab@rethab.ch>
2013-06-10 19:52:20 +0200

    added lzw blackbox

--- tests/Blackbox.hs
+++ tests/Blackbox.hs
@@ -6,6 +6,7 @@ import Test.QuickCheck hiding ((.&.))
 import Test.QuickCheck.All
 
 import Compression.Huffman
+import Compression.LZW
 
 import qualified Data.ByteString        as BS
 
@@ -14,6 +15,8 @@ main = $quickCheckAll
 instance Arbitrary BS.ByteString where
     arbitrary = fmap BS.pack arbitrary
 
-prop_reverse_function bs = not (BS.null bs) && BS.length bs `mod` 4 == 0  ==>
+prop_reverse_function_huff bs = not (BS.null bs) && BS.length bs `mod` 4 == 0  ==>
     (decode . encode) bs == bs
 
+prop_reverse_function_lzw bs = not (BS.null bs) && BS.length bs `mod` 4 == 0  ==>
+    (decompress . compress) bs == bs
